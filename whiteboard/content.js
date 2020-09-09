9172920a75c2379cc6a72531adffef0594f2e67e
Sunny Guan <sunny.peng.guan@gmail.com>
2020-09-08 22:56:09 -0500

    reformatted announcements + loading logci

--- whiteboard/content.js
+++ whiteboard/content.js
@@ -127,27 +127,46 @@ function processTemplate(template) {
     emailElement.innerText = email;
 }
 
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    return div.firstChild; 
+  }
+
 function waitForElementToDisplay(time) {
     if (document.querySelector("#bbFrame") != null && document.getElementById("bbFrame").contentDocument.getElementById("whatsNewModule") != null
         && document.getElementById("bbFrame").contentDocument.getElementById("whatsNewModule").textContent.length > 2000) {
         console.log(document.getElementById("bbFrame").contentDocument.getElementById("whatsNewModule").textContent);
         var ul = document.getElementById("bbFrame").contentDocument.getElementById("blocklist::2-whatsNewView:::::AN"); // => <a href="#">Link...
         var li = ul.getElementsByTagName("li");
+        var aul = document.getElementById("announcementUl");
         var idx = 0;
         for (var i of li) {
             var title = i.querySelector("span > a");
             var course = i.querySelector("span > div > a");
-            var elements = document.querySelectorAll(".announcement");
-            var element = elements[elements.length - 1];
-            var newElement = element.cloneNode(true);
+            var newElement = createElementFromHTML(`<li class="announcement mdl-list__item mdl-list__item--three-line">
+                                                        <span class="mdl-list__item-primary-content">
+                                                            <i class="material-icons mdl-list__item-avatar">person</i>
+                                                            <span class="announcementTitle">Sample</span>
+                                                            <span class="announcementContent mdl-list__item-text-body">
+                                                                Bryan Cranston played the role of Walter in Breaking Bad. He is also known
+                                                                for playing Hal in Malcom in the Middle.
+                                                            </span>
+                                                        </span>
+                                                        <span class="mdl-list__item-secondary-content">
+                                                            <a class="mdl-list__item-secondary-action" href="#"><i class="material-icons">star</i></a>
+                                                        </span>
+                                                    </li>`);
             newElement.querySelector(".announcementTitle").textContent = title.textContent
             newElement.querySelector(".announcementContent").textContent = course.textContent
-            element.insertAdjacentElement("afterend", newElement);
+            aul.appendChild(newElement);
             idx++;
-            if(idx > 3){
+            if (idx > 3) {
                 break;
             }
         }
+        document.querySelector("#announcementLoad").style.display = 'none';
+        aul.style.textAlign = "";
         return;
     }
     else {
