a08344038b11dc87ebe1fac751b97eacf5bcb1cd
Sunny Guan <sunny@utdallas.edu>
2022-09-23 22:57:42 -0500

    make event clickable

--- whiteboard/js/home.js
+++ whiteboard/js/home.js
@@ -43,7 +43,7 @@ function processAgenda() {
                 const id = cls.id;
                 let newElement = createElementFromHTML(`
                     <p class="zoomText employee design box" style="background-color: ${color}; font-size: 12px;">
-                        <a class="directLink" style="white-space: pre; text-decoration: none; color: white; cursor: inherit; width: 100%; height: 100%" 
+                        <a class="directLink" style="white-space: pre; text-decoration: none; color: white; cursor: inherit; width: 100%; height: 100%; display: block" 
                             href="${urlPrefix}/webapps/calendar/launch/attempt/_blackboard.platform.gradebook2.GradableItem-${id}">${name + "\n" + course}</a>
                     </p>
                 `);
